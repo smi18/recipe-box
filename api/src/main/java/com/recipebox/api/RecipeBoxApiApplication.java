9e94395eb22732aa0b11485fcd1bfa0e04b7bf16
smi18 <smitha.kurian@gmail.com>
2019-09-05 20:58:38 -0400

    Adding the Spring boot application for API

---
+++ api/src/main/java/com/recipebox/api/RecipeBoxApiApplication.java
@@ -0,0 +1,13 @@
+package com.recipebox.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeBoxApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeBoxApiApplication.class, args);
+	}
+
+}
